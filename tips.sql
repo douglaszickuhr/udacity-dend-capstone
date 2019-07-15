937bf58ca5452d47b8c07625b7ac6dc343d21fe4
Douglas Zickuhr <douglasrsl@gmail.com>
2019-07-15 16:01:48 +0100

    Update tips.sql

--- tips.sql
+++ tips.sql
@@ -3,6 +3,6 @@ CREATE TABLE staging_tips (user_id VARCHAR, business_id VARCHAR, text VARCHAR(65
 
 COPY staging_tips
 FROM 's3://udacity-dend-capstone-douglas/yelp_academic_dataset_tip.json' 
-ACCESS_KEY_ID 'AKIAYLC37GRFUUJWRDWP'
-SECRET_ACCESS_KEY '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'
+ACCESS_KEY_ID ''
+SECRET_ACCESS_KEY ''
 FORMAT AS JSON 'auto';
