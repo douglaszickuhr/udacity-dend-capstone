3211a0783e4e8b64108d17715586693b15d1facc
douglaszickuhr <douglasrsl@hotmail.com>
2019-07-27 10:10:37 +0100

    Removing the massive list definition and replacing by a clean yaml read.

--- dags/udacity-capstone-project-dz.py
+++ dags/udacity-capstone-project-dz.py
@@ -1,10 +1,10 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.subdag_operator import SubDagOperator
-from subdags.copy_to_redshift import get_s3_to_redshift
+# from airflow.operators.subdag_operator import SubDagOperator
+# from subdags.copy_to_redshift import get_s3_to_redshift
+from airflow.operators import S3ToRedshiftOperator
 import yaml
-#from airflow.operators import S3ToRedshiftOperator
 
 
 start_date = datetime.utcnow()
@@ -24,85 +24,26 @@ dag = DAG('udacity-dend-capstone-dz',
           description='Load and transform data in Redshift with Airflow',
           max_active_runs=1)
 
-start_operator = DummyOperator(
-    dag=dag,
-    task_id='start_operator'
-)
+# Read table definitions from YAML file
+with open('dags/configuration/copy_from_s3_to_redshift.yml', 'r') as file:
+    copy_definitions = yaml.safe_load(file)
 
-definitions = [{'table_name': 'tips',
-                's3_key': 'yelp_academic_dataset_tip.json',
-                'copy_params': ["JSON 'auto'"],
-                'origin_schema': [{'name': 'user_id', 'type': 'varchar'},
-                                  {'name': 'business_id', 'type': 'varchar'},
-                                  {'name': 'text', 'type': 'varchar(65535)'},
-                                  {'name': 'date', 'type': 'timestamp'},
-                                  {'name': 'compliment_count', 'type': 'int4'}]},
-               {'table_name': 'business',
-                's3_key': 'yelp_academic_dataset_business_short.csv',
-                'copy_params': ['FORMAT AS CSV', "IGNOREHEADER 1"],
-                'origin_schema': [{'name': 'business_id', 'type': 'varchar'},
-                                  {'name': 'name', 'type': 'varchar'},
-                                  {'name': 'categories', 'type': 'varchar'},
-                                  {'name': 'type', 'type': 'varchar'},
-                                  {'name': 'latitude', 'type': 'float8'},
-                                  {'name': 'longitude', 'type': 'float8'},
-                                  {'name': 'review_count', 'type': 'int4'},
-                                  {'name': 'state', 'type': 'varchar'},
-                                  {'name': 'city', 'type': 'varchar'},
-                                  {'name': 'full_address', 'type': 'varchar(65535)'},
-                                  {'name': 'open', 'type': 'bool'}]},
-               {'table_name': 'users',
-                's3_key': 'yelp_academic_dataset_user.json',
-                'copy_params': ["JSON 'auto'"],
-                'origin_schema': [{'name': 'user_id', 'type': 'varchar'},
-                                  {'name': 'name', 'type': 'varchar'},
-                                  {'name': 'review_count', 'type': 'int4'},
-                                  {'name': 'yelping_since', 'type': 'datetime'},
-                                  {'name': 'useful', 'type': 'int4'},
-                                  {'name': 'funny', 'type': 'int4'},
-                                  {'name': 'cool', 'type': 'int4'},
-                                  {'name': 'elite', 'type': 'varchar'},
-                                  {'name': 'fans', 'type': 'int4'},
-                                  {'name': 'average_stars', 'type': 'float8'},
-                                  {'name': 'compliment_hot', 'type': 'int4'},
-                                  {'name': 'compliment_more', 'type': 'int4'},
-                                  {'name': 'compliment_profile', 'type': 'int4'},
-                                  {'name': 'compliment_cute', 'type': 'int4'},
-                                  {'name': 'compliment_list', 'type': 'int4'},
-                                  {'name': 'compliment_note', 'type': 'int4'},
-                                  {'name': 'compliment_plain', 'type': 'int4'},
-                                  {'name': 'compliment_cool', 'type': 'int4'},
-                                  {'name': 'compliment_funny', 'type': 'int4'},
-                                  {'name': 'compliment_writer', 'type': 'int4'},
-                                  {'name': 'compliment_photos', 'type': 'int4'}]},
-               {'table_name': 'reviews',
-                's3_key': 'yelp_academic_dataset_review.json',
-                'copy_params': ["JSON 'auto'"],
-                'origin_schema': [{'name': 'review_id', 'type': 'varchar'},
-                                  {'name': 'user_id', 'type': 'varchar'},
-                                  {'name': 'business_id', 'type': 'varchar'},
-                                  {'name': 'stars', 'type': 'float8'},
-                                  {'name': 'useful', 'type': 'int4'},
-                                  {'name': 'funny', 'type': 'int4'},
-                                  {'name': 'cool', 'type': 'int4'},
-                                  {'name': 'text', 'type': 'varchar(max)'},
-                                  {'name': 'date', 'type': 'datetime'}]}]
+with dag:
+    start_operator = DummyOperator(
+        task_id='start_operator')
 
-subdag_id = 'copy_data_to_redshift_subdag'
-copy_data_to_redshift = SubDagOperator(
-    subdag=get_s3_to_redshift(
-        parent_dag_name='udacity-dend-capstone-dz',
-        task_id=subdag_id,
-        tables_definition=definitions,
-        redshift_conn_id='redshift',
-        redshift_schema='public',
-        s3_conn_id='aws_credentials',
-        s3_bucket='udac-dend-capstone-dz',
-        load_type='truncate',
-        schema_location='Local',
-        start_date=start_date),
-    task_id=subdag_id,
-    dag=dag,
-)
+    for table in copy_definitions:
+        copy_task = S3ToRedshiftOperator(
+            task_id=f"copy_{table.get('table_name',None)}_to_redshift",
+            redshift_conn_id='redshift',
+            redshift_schema='public',
+            table=f"staging_{table.get('table_name',None)}",
+            s3_conn_id='aws_credentials',
+            s3_bucket='udac-dend-capstone-dz',
+            s3_key=table.get('s3_key', None),
+            load_type='truncate',
+            copy_params=table.get('copy_params', None),
+            origin_schema=table.get('origin_schema', None),
+            schema_location='Local')
 
-start_operator >> copy_data_to_redshift
+        copy_task.set_upstream(start_operator)
