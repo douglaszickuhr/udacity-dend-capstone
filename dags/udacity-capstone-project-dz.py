8a10b34ecae20390fb9dee1dff1c7f9437348bcd
douglaszickuhr <douglasrsl@hotmail.com>
2019-07-28 12:55:17 +0100

    Migrating copy task to subdag. Adding Dimension processing

--- dags/udacity-capstone-project-dz.py
+++ dags/udacity-capstone-project-dz.py
@@ -1,11 +1,11 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.dummy_operator import DummyOperator
-# from airflow.operators.subdag_operator import SubDagOperator
-# from subdags.copy_to_redshift import get_s3_to_redshift
-from airflow.operators import S3ToRedshiftOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from subdags.copy_to_redshift import get_s3_to_redshift
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.executors import GetDefaultExecutor
 import yaml
-
 
 start_date = datetime.utcnow()
 
@@ -16,7 +16,8 @@ default_args = {
     'retry_delay': timedelta(minutes=3),
     'catchup': False,
     'email_on_retry': False,
-    'start_date': start_date
+    'start_date': start_date,
+    'template_searchpath': './dags'
 }
 
 dag = DAG('udacity-dend-capstone-dz',
@@ -28,22 +29,34 @@ dag = DAG('udacity-dend-capstone-dz',
 with open('dags/configuration/copy_from_s3_to_redshift.yml', 'r') as file:
     copy_definitions = yaml.safe_load(file)
 
+with open('dags/configuration/dimensions.yml', 'r') as file:
+    dimensions_definitions = yaml.safe_load(file)
+
 with dag:
     start_operator = DummyOperator(
         task_id='start_operator')
 
-    for table in copy_definitions:
-        copy_task = S3ToRedshiftOperator(
-            task_id=f"copy_{table.get('table_name',None)}_to_redshift",
+    subdag_id = 'copy_data_to_redshift'
+    copy_data_to_redshift = SubDagOperator(
+        subdag=get_s3_to_redshift(
+            parent_dag_name='udacity-dend-capstone-dz',
+            task_id=subdag_id,
+            tables_definition=copy_definitions,
             redshift_conn_id='redshift',
             redshift_schema='public',
-            table=f"staging_{table.get('table_name',None)}",
             s3_conn_id='aws_credentials',
             s3_bucket='udac-dend-capstone-dz',
-            s3_key=table.get('s3_key', None),
-            load_type='rebuild',
-            copy_params=table.get('copy_params', None),
-            origin_schema=table.get('origin_schema', None),
-            schema_location='Local')
+            load_type='truncate',
+            schema_location='Local',
+            start_date=start_date),
+        task_id=subdag_id,
+        dag=dag,
+        executor=GetDefaultExecutor())
+    copy_data_to_redshift.set_upstream(start_operator)
 
-        copy_task.set_upstream(start_operator)
+    for dimension in dimensions_definitions:
+        dimension_task = PostgresOperator(
+            task_id=f"process_{dimension.get('task_name')}",
+            sql=dimension.get('sql', None),
+            postgres_conn_id='redshift')
+        dimension_task.set_upstream(copy_data_to_redshift)
